f827fea3ac1ace87fe26d26da40e0eb83c494b5c
Jordan Esh <esh.jordan@gmail.com>
2021-09-22 03:22:35 +1000

    Added RTOS+TCP extension

--- src/System.cpp
+++ src/System.cpp
@@ -6,10 +6,22 @@ void init(void)
 {
     if (!System::Impl::initialised)
     {
+
+#ifdef ECU_ENABLE_NETWORKING
+        /* Initialise the RTOS's TCP/IP stack.  The tasks that use the network
+        are created in the vApplicationIPNetworkEventHook() hook function
+        below.  The hook function is called when the network connects. */
+        FreeRTOS_IPInit(ucIPAddress, ucNetMask, ucGatewayAddress, ucDNSServerAddress, ucMACAddress);
+#endif
+
         System::Impl::ParameterList::add_parameter("position", 0.0);
         System::Impl::ParameterList::add_parameter("velocity", 0.0);
         System::Impl::ParameterList::add_parameter("acceleration", 1.0);
+
+        /* Start the RTOS schedule, set tasks and timers running. */
+        vTaskStartScheduler();
     }
+
     System::Impl::initialised = true;
 }
 
--- src/main.cpp
+++ src/main.cpp
@@ -24,9 +24,6 @@ int main(int argc, char *argv[])
     /* Add tasks here. */
     System::init();
 
-    /* Start the tasks and timer running. */
-    vTaskStartScheduler();
-
     /* If all is well, the scheduler will now be running, and the following
      * line will never be reached.  If the following line does execute, then
      * there was insufficient FreeRTOS heap memory available for the idle and/or
